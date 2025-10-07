9635407f52fe1d4766d9a78cca7127f9fb00fea7
Ronald <ronald@rangel.pro>
2025-10-07 02:01:10 -0500

    gaa2

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,7 @@ import { useEffect } from "react"
 import { useRouter } from "next/navigation"
 
 export default function Home() {
-  const router = useRouter()
 
-  useEffect(() => {
-    router.push("/dashboard")
-  }, [router])
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
